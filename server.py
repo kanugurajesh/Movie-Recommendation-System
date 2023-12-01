ef581310831ce58f479c5b3898989fb8333dab6a
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-01 10:23:41 +0530

    pushing the cleaned code

--- server.py
+++ server.py
@@ -43,26 +43,10 @@ def fetch_poster(movie_id):
 def read_root():
     return movies['title'].values.tolist()
 
-# @app.get("/recommend/{movie}")
-# def recommend(movie):
-#     index = movies[movies['title'] == movie].index[0]
-#     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-#     recommended_movie_names = []
-#     recommended_movie_posters = []
-#     for i in distances[1:6]:
-#         # fetch the movie poster
-#         movie_id = movies.iloc[i[0]].movie_id
-#         recommended_movie_posters.append(fetch_poster(movie_id))
-#         recommended_movie_names.append(movies.iloc[i[0]].title)
-
-#     return [recommended_movie_names,recommended_movie_posters]
-
 @app.get("/recommend/{movie}")
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    # recommended_movie_names = []
-    # recommended_movie_posters = []
     recommended_movie_data = {}
     for i in distances[1:20]:
         # fetch the movie poster
